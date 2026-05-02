58a598c84a17daf6669bdf544c36cd5fe58f4184
Assassin859 <maitreyagaikwad859@gmail.com>
2026-05-02 16:27:02 +0530

    Fix: Use REDIS_URL for cloud deployment

--- Backend/Queue/redis.js
+++ Backend/Queue/redis.js
@@ -1,8 +1,13 @@
 import IORedis from "ioredis";
 
-export const redisConnection = new IORedis({
-  host: "127.0.0.1",
-  port: 6379,
-  maxRetriesPerRequest: null
-}); 
+import dotenv from "dotenv";
+dotenv.config();
+
+export const redisConnection = process.env.REDIS_URL 
+  ? new IORedis(process.env.REDIS_URL, { maxRetriesPerRequest: null })
+  : new IORedis({
+      host: "127.0.0.1",
+      port: 6379,
+      maxRetriesPerRequest: null
+    }); 
 
